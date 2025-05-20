a239eb1fab82c603a32189a3923a4b3aba5972bf
happy <anousone9102@gmail.com>
2025-05-20 18:53:33 +0700

    Initial commit

--- actions/auth.ts
+++ actions/auth.ts
@@ -9,7 +9,7 @@ export async function loginAction() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `http://localhost:3000/auth/callback`,
+      redirectTo: `https://fast-quiz-kappa.vercel.app/auth/callback`,
       scopes: "https://www.googleapis.com/auth/forms.body",
     },
   });
