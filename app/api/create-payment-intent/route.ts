57704a553213a41153a9034c1c5b9b4d8cc26c8c
happy <anousone9102@gmail.com>
2025-06-10 15:43:45 +0700

    Initial commit

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,23 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
-import Stripe from "stripe";
+// import { NextRequest, NextResponse } from "next/server";
+// import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-export async function POST(req: NextRequest) {
-  try {
-    const { amount } = await req.json();
+// export async function POST(req: NextRequest) {
+//   try {
+//     const { amount } = await req.json();
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: "thb",
-    });
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: amount,
+//       currency: "thb",
+//     });
 
-    return NextResponse.json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    console.log("Internal server error", (error as Error).message);
-    return NextResponse.json(
-      { error: `Internal server error ${error}` },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ clientSecret: paymentIntent.client_secret });
+//   } catch (error) {
+//     console.log("Internal server error", (error as Error).message);
+//     return NextResponse.json(
+//       { error: `Internal server error ${error}` },
+//       { status: 500 }
+//     );
+//   }
+// }
