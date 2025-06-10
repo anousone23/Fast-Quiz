fa993d78629b3af3f2aff0dc9e392f252ccaae24
happy <anousone9102@gmail.com>
2025-06-10 16:02:27 +0700

    Initial commit

--- app/(root)/checkout/page.tsx
+++ app/(root)/checkout/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import generatePayload from "promptpay-qr";
 import { QRCodeSVG } from "qrcode.react";
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, Suspense, useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
 import { RxCross2 } from "react-icons/rx";
 import { ClipLoader } from "react-spinners";
@@ -75,68 +75,73 @@ export default function CheckoutPage() {
   const qr = generatePayload(phone, { amount });
 
   return (
-    <div className="w-full h-screen px-8 py-8 flex flex-col">
-      <Header />
+    <Suspense fallback={<ClipLoader size={32} color="#fafafa" />}>
+      <div className="w-full h-screen px-8 py-8 flex flex-col">
+        <Header />
 
-      <main className="flex-1 flex flex-col items-center justify-center gap-y-4">
-        <p>Total amount: {amount} BAHT</p>
+        <main className="flex-1 flex flex-col items-center justify-center gap-y-4">
+          <p>Total amount: {amount} BAHT</p>
 
-        <div className="bg-zinc-100 px-4 py-4 rounded-md flex flex-col items-center gap-y-4">
-          <p className="text-black font-medium">PromptPay</p>
+          <div className="bg-zinc-100 px-4 py-4 rounded-md flex flex-col items-center gap-y-4">
+            <p className="text-black font-medium">PromptPay</p>
 
-          <div className="flex flex-col items-center gap-y-4">
-            <QRCodeSVG value={qr} size={140} />
+            <div className="flex flex-col items-center gap-y-4">
+              <QRCodeSVG value={qr} size={140} />
 
-            {/* image input */}
-            {imageFile ? (
-              <div className="border border-zinc-300 w-full px-4 py-2 rounded-sm bg-zinc-100 flex flex-row items-center gap-x-4">
-                <p className="text-xs md:text-sm text-black flex-1">
-                  {imageFile.name}
-                </p>
-
-                <RxCross2
-                  className="text-black cursor-pointer"
-                  size={18}
-                  onClick={() => setImageFile(null)}
-                />
-              </div>
-            ) : (
-              <>
-                <p className="text-xs md:text-sm text-orange-500">
-                  Attach the payment image after paid
-                </p>
-
-                <div
-                  className="border border-zinc-300 w-full px-4 py-2 rounded-sm bg-zinc-100 cursor-pointer"
-                  onClick={() => {
-                    if (fileRef.current) fileRef.current.click();
-                  }}
-                >
-                  <p className="text-xs md:text-sm text-black">
-                    Upload payment image
+              {/* image input */}
+              {imageFile ? (
+                <div className="border border-zinc-300 w-full px-4 py-2 rounded-sm bg-zinc-100 flex flex-row items-center gap-x-4">
+                  <p className="text-xs md:text-sm text-black flex-1">
+                    {imageFile.name}
                   </p>
+
+                  <RxCross2
+                    className="text-black cursor-pointer"
+                    size={18}
+                    onClick={() => setImageFile(null)}
+                  />
                 </div>
+              ) : (
+                <>
+                  <p className="text-xs md:text-sm text-orange-500">
+                    Attach the payment image after paid
+                  </p>
 
-                <Input
-                  ref={fileRef}
-                  type="file"
-                  hidden
-                  accept="image/*"
-                  onChange={handleFileChange}
-                />
-              </>
-            )}
+                  <div
+                    className="border border-zinc-300 w-full px-4 py-2 rounded-sm bg-zinc-100 cursor-pointer"
+                    onClick={() => {
+                      if (fileRef.current) fileRef.current.click();
+                    }}
+                  >
+                    <p className="text-xs md:text-sm text-black">
+                      Upload payment image
+                    </p>
+                  </div>
 
-            <Button
-              className="w-full bg-zinc-700 border-zinc-600 hover:bg-zinc-600 "
-              onClick={handleConfirmPayment}
-            >
-              {isLoading ? <ClipLoader size={20} color="#fafafa" /> : "Confirm"}
-            </Button>
+                  <Input
+                    ref={fileRef}
+                    type="file"
+                    hidden
+                    accept="image/*"
+                    onChange={handleFileChange}
+                  />
+                </>
+              )}
+
+              <Button
+                className="w-full bg-zinc-700 border-zinc-600 hover:bg-zinc-600 "
+                onClick={handleConfirmPayment}
+              >
+                {isLoading ? (
+                  <ClipLoader size={20} color="#fafafa" />
+                ) : (
+                  "Confirm"
+                )}
+              </Button>
+            </div>
           </div>
-        </div>
 
-        {/* <Elements
+          {/* <Elements
           stripe={stripePromise}
           options={{
             mode: "payment",
@@ -146,7 +151,8 @@ export default function CheckoutPage() {
         >
           <CheckOut amount={amount} />
         </Elements> */}
-      </main>
-    </div>
+        </main>
+      </div>
+    </Suspense>
   );
 }
